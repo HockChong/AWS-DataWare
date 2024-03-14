df3bd177bc3e00546f39834f39dea8b769946471
Hock Chong <64703507+HockChong@users.noreply.github.com>
2024-03-14 23:20:10 +0800

    Update etl.py

--- etl.py
+++ etl.py
@@ -4,6 +4,16 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Loads data into staging tables.
+
+    Args:
+        cur: The cursor object for executing SQL queries.
+        conn: The connection object to the database.
+
+    Returns:
+        None
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
@@ -15,17 +25,47 @@ def insert_tables(cur, conn):
         conn.commit()
 
 
+import configparser
+import psycopg2
+
 def main():
+    """
+    Main function that executes the ETL process.
+    
+    This function reads the configuration file, establishes a connection to the database,
+    and calls the necessary functions to load staging tables and insert data into the final tables.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+
     cur = conn.cursor()
     
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
     conn.close()
+
+def load_staging_tables(cur, conn):
+    """
+    Loads data from source files into staging tables.
+    
+    Args:
+        cur: Cursor object for executing SQL statements.
+        conn: Connection object for the database.
+    """
+    # Implementation goes here
+
+def insert_tables(cur, conn):
+    """
+    Inserts data from staging tables into final tables.
+    
+    Args:
+        cur: Cursor object for executing SQL statements.
+        conn: Connection object for the database.
+    """
+    # Implementation goes here
 
 
 if __name__ == "__main__":
